46773d03a8c4feca940be07fba72490e3ca6d52b
Thomas Letan <lthms@soap.coffee>
2026-08-22 23:37:49 +0200

    Move the control plane database and PKI in an external volume (#15)

--- locals.tf
+++ locals.tf
@@ -12,6 +12,16 @@ locals {
   control_plane_server_type = "cx23"
   control_plane_location    = "hel1"
 
+  # 10 GB is Hetzner's floor (they go 10 GB to 10 TB in 1 GB steps), and generous
+  # for a single-node sqlite datastore plus the PKI:
+  # https://docs.hetzner.com/cloud/volumes/overview
+  control_plane_volume_size = 10
+
+  # Precomputed instead of reading hcloud_volume_attachement.control_plane_k3s.linux_device,
+  # to avoid a circular dependency (we need to know it _before_ that attachement).
+  # https://docs.hetzner.cloud/reference/cloud#volumes
+  control_plane_volume_device = "/dev/disk/by-id/scsi-0HC_Volume_${hcloud_volume.control_plane_k3s.id}"
+
   network_zone     = "eu-central"
   network_ip_range = "10.0.0.0/16"
   nodes_ip_range   = "10.0.1.0/24"
